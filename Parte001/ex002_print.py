1a238f654da886871d61a5eda3d147310a456083
Alberto Madin Rivera <88264505+AlbertoMadinRivera@users.noreply.github.com>
2023-12-25 22:20:02 -0600

    Update ex002_print.py
    En esta versión, he agregado comentarios para explicar cada sección del código y mejorar la comprensión. También he organizado el código para que sea más claro y legible. Además, he separado las secciones relacionadas, como los diferentes usos de `print`, con líneas en blanco para mejorar la estructura visual del código.

--- Parte001/ex002_print.py
+++ Parte001/ex002_print.py
@@ -1,27 +1,26 @@
 # Ejercicio 2: Exponer el uso básico de la función print.
 
+# Ejemplo básico
 print('Este es un ejemplo básico.')
 
+# Concatenar sin espacio entre las cadenas
 print('Este es un ejemplo básico.', end='')
 print('Este es un ejemplo básico.', end='')
 
+# Imprimir una línea en blanco
 print()
 
+# Imprimir palabras separadas por espacio y guión
 print('Python', 'es', 'tremendo!')
 print('Python', 'es', 'tremendo!', sep='-')
 
-print()
-
+# Imprimir usando format
 print('{} es {}'.format('Python', 'tremendo!'))
 
-print()
-
+# Imprimir una lista
 numeros = [2, 3, 5, 7, 11]
-
 print(numeros)
 
-print()
-
+# Imprimir un diccionario
 capitales = {'Colombia': 'Bogotá', 'Perú': 'Lima'}
-
 print(capitales)
